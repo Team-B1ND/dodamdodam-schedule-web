209a7ed6d9d873a710eb6b081b00336676cede6a
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-07-01 11:51:09 +0900

    :bug: :: 학사일정 장소 네이밍 변경 #12

--- src/hooks/schedule/useCalendarSchedules.ts
+++ src/hooks/schedule/useCalendarSchedules.ts
@@ -3,17 +3,11 @@ import { useGetSchedulesByDate } from "../../queries/schedule/schedule.query";
 import dayjs from "dayjs";
 import dataTransform from "../../util/transform/dataTransform";
 import { useRecoilValue } from "recoil";
-import {
-  scheduleClassificationKeyword,
-  scheduleDateAtom,
-} from "../../store/schedule/schedule.store";
+import { scheduleClassificationKeyword, scheduleDateAtom } from "../../store/schedule/schedule.store";
 import { Schedule } from "../../types/schedule/schedule.type";
 import { useGetMember } from "../../queries/member/member.query";
 import token from "../../lib/token/token";
-import {
-  ACCESS_TOKEN_KEY,
-  REFRESH_TOKEN_KEY,
-} from "../../constants/token/token.constant";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../../constants/token/token.constant";
 
 const useCalendarSchedules = () => {
   const date = useRecoilValue(scheduleDateAtom);
@@ -33,21 +27,21 @@ const useCalendarSchedules = () => {
     if (classificationKeyword === "내 일정") {
       newSchedulesData = newSchedulesData.filter(
         (schedule) =>
-          schedule.targetGrades[0].indexOf(String(memberData?.data.classroom.grade)) >
-            -1 || schedule.targetGrades[0] === "전교생"
+          schedule.targetGrades[0].indexOf(String(memberData?.data.classroom.grade)) > -1 ||
+          schedule.targetGrades[0] === "전교생",
       );
     }
     newSchedulesData!.forEach((schedule) =>
       setHandleSchedule((prev) => {
         const newHandleCalendarSchedule = calendarScheduleTransform(schedule);
         return [...prev, newHandleCalendarSchedule];
-      })
+      }),
     );
   }, [classificationKeyword, memberData?.data?.classroom?.grade, schedulesData]);
 
   const calendarScheduleTransform = (schedule: Schedule) => {
     const scheduleColor = dataTransform.scheduleTargetTransform(
-       dataTransform.scheduleTargetDataTransform(schedule.targetGrades[0])
+      dataTransform.scheduleTargetDataTransform(schedule.targetGrades[0]),
     );
 
     const newHandleSchedule = {
@@ -55,7 +49,7 @@ const useCalendarSchedules = () => {
       title: schedule.name,
       target: schedule.targetGrades[0],
       attendees: [dataTransform.scheduleTargetDataTransform(schedule.targetGrades[0])],
-      location: schedule.place === "ETC" ? "No Place" : schedule.place,
+      location: schedule.place === "ETC" ? "기타" : dataTransform.schedulePlaceTransform(schedule.place),
       category: "time",
       isReadOnly: true,
       borderColor: scheduleColor,
@@ -77,10 +71,7 @@ const useCalendarSchedules = () => {
   }, [schedulesData?.data, isLoading, loadCalendarSchedule]);
 
   useEffect(() => {
-    if (
-      !token.getToken(ACCESS_TOKEN_KEY) ||
-      !token.getToken(REFRESH_TOKEN_KEY)
-    ) {
+    if (!token.getToken(ACCESS_TOKEN_KEY) || !token.getToken(REFRESH_TOKEN_KEY)) {
       window.alert("토큰이 위조 됐습니다");
       window.location.href = "http://dodam.b1nd.com/sign";
     }
