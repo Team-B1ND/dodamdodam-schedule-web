49bcbb004d70067fb6cd5398b33b03dc45aac452
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2024-11-18 17:10:20 +0900

    chore :: ignore eslint

--- craco.config.js
+++ craco.config.js
@@ -2,4 +2,11 @@ module.exports = {
   eslint: {
     enable: false,
   },
+  webpack: {
+    resolve: {
+      fallback: {
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+  },
 };
