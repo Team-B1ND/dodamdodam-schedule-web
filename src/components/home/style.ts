4988300793b99aaa3e54d2d5b24b3d4f670303b6
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2025-02-20 19:43:21 +0900

    remove :: not use file delete #24

--- src/components/home/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const HomeContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  background-color: ${({ theme }) => theme.backgroundNormal}
-`;
