31a8a835c5165bedc62c2e0a4d379a9ad4324ed7
전민찬 <chan2bo2@dgsw.hs.kr>
2025-03-09 03:41:17 +0900

    fix :: lint error

--- src/hooks/Schedule/useSchedule.ts
+++ src/hooks/Schedule/useSchedule.ts
@@ -1,51 +1,51 @@
-import dayjs from "dayjs";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { useRecoilState } from "recoil";
-import { scheduleDateAtom } from "src/store/Schedule/schedule.store";
-import { useGetSchedulesByDate } from "src/queries/Schedule/schedule.query";
-import { eventSchedule, Schedule } from "src/types/Schedule/schedule.type";
-import dataTransform from "src/util/Transform/dataTransform";
-import ToastUIReactCalendar from "@toast-ui/react-calendar";
+import dayjs from 'dayjs'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { scheduleDateAtom } from 'src/store/Schedule/schedule.store'
+import { useGetSchedulesByDate } from 'src/queries/Schedule/schedule.query'
+import { eventSchedule, Schedule } from 'src/types/Schedule/schedule.type'
+import dataTransform from 'src/util/Transform/dataTransform'
+import ToastUIReactCalendar from '@toast-ui/react-calendar'
 
 export const useSchedule = () => {
-  const calendarRef = useRef<ToastUIReactCalendar>(null);
-  const [date, setDate] = useRecoilState(scheduleDateAtom);
-  const [schedule, setSchedule] = useState<eventSchedule[]>([]);
+  const calendarRef = useRef<ToastUIReactCalendar>(null)
+  const [date, setDate] = useRecoilState(scheduleDateAtom)
+  const [schedule, setSchedule] = useState<eventSchedule[]>([])
   const [sortScheduleGroup, setSortScheduleGroup] = useState<
     Record<string, typeof schedule>
-  >({});
+  >({})
 
   const { data: scheduleData, isLoading } = useGetSchedulesByDate({
     startAt: date,
     endAt: `${date.slice(0, 8)}${dayjs(date).daysInMonth()}`,
-  });
+  })
 
   const handleChangeDate = (type: string) => {
-    const calendarInstance = calendarRef!.current!.getInstance();
+    const calendarInstance = calendarRef!.current!.getInstance()
 
-    if (type === "increase") {
-      calendarInstance?.next();
-      setDate(dayjs(date).add(1, "month").format("YYYY-MM-DD"));
+    if (type === 'increase') {
+      calendarInstance?.next()
+      setDate(dayjs(date).add(1, 'month').format('YYYY-MM-DD'))
     } else {
-      calendarInstance?.prev();
-      setDate(dayjs(date).subtract(1, "month").format("YYYY-MM-DD"));
+      calendarInstance?.prev()
+      setDate(dayjs(date).subtract(1, 'month').format('YYYY-MM-DD'))
     }
-  };
+  }
 
   const calendarScheduleLoad = useCallback(() => {
-    scheduleData?.data.map((schedule) => {
+    scheduleData?.data.forEach((schedule) => {
+      // forEach로 변경
       setSchedule((prev) => {
-        const newCalendarData = calendarScheduleTransform(schedule);
-
-        return [...prev, newCalendarData];
-      });
-    });
-  }, [scheduleData?.data]);
+        const newCalendarData = calendarScheduleTransform(schedule)
+        return [...prev, newCalendarData]
+      })
+    })
+  }, [scheduleData?.data])
 
   const calendarScheduleTransform = (schedule: Schedule) => {
     const eventScheduleColor = dataTransform.scheduleTargetTransform(
       dataTransform.scheduleTargetDataTransform(schedule.targetGrades[0])
-    );
+    )
 
     const newCalendarScheduleData = {
       id: schedule.id,
@@ -55,46 +55,46 @@ export const useSchedule = () => {
         dataTransform.scheduleTargetDataTransform(schedule.targetGrades[0]),
       ],
       location: dataTransform.schedulePlaceTransform(schedule.place),
-      category: "time",
+      category: 'time',
       isReadOnly: true,
       borderColor: eventScheduleColor,
       backgroundColor: eventScheduleColor,
       start: schedule.date[0],
       end: schedule.date[1],
       state: null,
-    };
+    }
 
-    return newCalendarScheduleData;
-  };
+    return newCalendarScheduleData
+  }
 
   useEffect(() => {
-    setSchedule([]);
+    setSchedule([])
 
     if (scheduleData?.data && !isLoading) {
-      calendarScheduleLoad();
+      calendarScheduleLoad()
     }
-  }, [scheduleData?.data, isLoading, calendarScheduleLoad, date]);
+  }, [scheduleData?.data, isLoading, calendarScheduleLoad, date])
 
   useEffect(() => {
     const scheduleGroup = schedule.reduce((acc, item) => {
-      const key = item.start;
+      const key = item.start
 
       if (!acc[key]) {
-        acc[key] = [];
+        acc[key] = []
       }
-      acc[key].push(item);
-      return acc;
-    }, {} as Record<string, typeof schedule>);
+      acc[key].push(item)
+      return acc
+    }, {} as Record<string, typeof schedule>)
 
     const sortScheduleGroup = Object.keys(scheduleGroup)
       .sort((a, b) => new Date(a).getTime() - new Date(b).getTime()) // 날짜 순으로 정렬
       .reduce((acc, key) => {
-        acc[key] = scheduleGroup[key];
-        return acc;
-      }, {} as Record<string, typeof schedule>);
+        acc[key] = scheduleGroup[key]
+        return acc
+      }, {} as Record<string, typeof schedule>)
 
-    setSortScheduleGroup(sortScheduleGroup);
-  }, [schedule]);
+    setSortScheduleGroup(sortScheduleGroup)
+  }, [schedule])
 
   return {
     date,
@@ -102,5 +102,5 @@ export const useSchedule = () => {
     sortScheduleGroup,
     calendarRef,
     handleChangeDate,
-  };
-};
+  }
+}
